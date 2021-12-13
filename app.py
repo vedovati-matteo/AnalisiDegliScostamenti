c9a43803cb9fec07ffca59094b4376e54731e0d7
vedovati-matteo <68272450+vedovati-matteo@users.noreply.github.com>
2021-12-13 18:36:00 +0100

    up

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+prova = "Ciao"
 
 @app.route("/")
 def hello_world():
-    return render_template("home.html")
+    return render_template("home.html", p=(('c', 'a', 'c', 'a'),('p','i','p','p')))
 
 if __name__ == '__main__':
 	app.run(host='127.0.0.1', port=5000, debug=True)
