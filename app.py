ada378f4bdf31f1358ff350d9a379f32852961dd
LucaCapelli <l.capelli11@studenti.unibg.it>
2021-12-22 19:32:49 +0100

    primo

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 prova = "Ciao"
 
 @app.route("/")
-def hello_world():
-    return render_template("home.html", p=(('c', 'a', 'c', 'a'),('p','i','p','p')))
+def index():
+    return render_template("home.html", p=((407000,67840,242200,80000,16960),(366300,61056,217000,80000,7264),(363500,60160,217000,80000,6340),(370000,63920,220780,80000,5300)))
 
 if __name__ == '__main__':
 	app.run(host='127.0.0.1', port=5000, debug=True)
