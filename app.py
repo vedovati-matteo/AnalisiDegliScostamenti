cd23bab02cf79f147844c9188cf8fc93d9009407
LucaCapelli <l.capelli11@studenti.unibg.it>
2021-12-23 17:46:48 +0100

    wq

--- app.py
+++ app.py
@@ -8,5 +8,14 @@ prova = "Ciao"
 def index():
     return render_template("home.html", p=((407000,67840,242200,80000,16960),(366300,61056,217000,80000,7264),(363500,60160,217000,80000,6340),(370000,63920,220780,80000,5300)))
 
+@app.route("/listaArticoliFinali")
+def listaArticoliFinali():
+    return render_template("listaArticoliFinali.html", p=(("ART000025",15),("ART000525",17),("ART000016",528),("ART005118",-288),("ART000845",5),("ART000954",-238),("ART000231",34)))
+
+@app.route("/articoloSpecifico")
+def articoloSpecifico():
+    return render_template("articoloSpecifico.html", p=(("ART000025",15),("ART000525",17),("ART000016",528),("ART005118",-288),("ART000845",5),("ART000954",-238),("ART000231",34)))
+
+
 if __name__ == '__main__':
 	app.run(host='127.0.0.1', port=5000, debug=True)
