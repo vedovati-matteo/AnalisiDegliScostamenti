c24a38b258525805b1f6ec6c53cf01a952e9b6f5
LucaCapelli <l.capelli11@studenti.unibg.it>
2021-12-23 19:46:24 +0100

    wella

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def listaArticoliFinali():
 
 @app.route("/articoloSpecifico")
 def articoloSpecifico():
-    return render_template("articoloSpecifico.html", p=(("ART000025",15),("ART000525",17),("ART000016",528),("ART005118",-288),("ART000845",5),("ART000954",-238),("ART000231",34)))
+    return render_template("articoloSpecifico.html", p=((407000,67840,242200,80000,16960),(366300,61056,217000,80000,7264),(363500,60160,217000,80000,6340),(370000,63920,220780,80000,5300)))
 
 
 if __name__ == '__main__':
