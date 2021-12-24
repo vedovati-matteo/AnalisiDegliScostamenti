813b24898889c0408e872c363652fa084b38b562
vedovati-matteo <68272450+vedovati-matteo@users.noreply.github.com>
2021-12-24 15:20:07 +0100

    collegamento analisi di scostamento

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-prova = "Ciao"
+exec(open('analisiScostamenti.py').read())
 
 @app.route("/")
 def index():
-    return render_template("home.html", p=((407000,67840,242200,80000,16960),(366300,61056,217000,80000,7264),(363500,60160,217000,80000,6340),(370000,63920,220780,80000,5300)))
+    return render_template("home.html", p=tabellaScostamenti)
 
 @app.route("/listaArticoliFinali")
 def listaArticoliFinali():
