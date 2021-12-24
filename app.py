c617c6ed320c1f777b70c2a757dd33c5fb5ffc4e
LucaCapelli <l.capelli11@studenti.unibg.it>
2021-12-24 18:39:13 +0100

    asdassadfasf

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
-from flask import Flask, render_template, request
-import pandas as pd
-import functions as fun
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-exec(open('analisiScostamenti.py').read())
+#exec(open('analisiScostamenti.py').read())
 
 @app.route("/")
 def index():
@@ -12,21 +10,23 @@ def index():
 
 @app.route("/analisiScostamenti")
 def analisiScostamenti():
-    return render_template("analisiScostamenti.html", p=tabellaScostamenti)
+    return render_template("analisiScostamenti.html", p=((407000,67840,242200,80000,16960),(366300,61056,217000,80000,7264),(363500,60160,217000,80000,6340),(370000,63920,220780,80000,5300)))
 
 @app.route("/listaArticoliFinali")
 def listaArticoliFinali():
-    return render_template("listaArticoliFinali.html", p=artScost.values.tolist())
+    return render_template("listaArticoliFinali.html", p=(("ART000025",15),("ART000525",17),("ART000016",528),("ART005118",-288),("ART000845",5),("ART000954",-238),("ART000231",34)))
 
-@app.route("/articoloSpecifico", methods=['GET'])
+@app.route("/articoloSpecifico")
 def articoloSpecifico():
-    nrArticolo = request.args.get('codiceArticolo')
-    return render_template("articoloSpecifico.html", nrArticolo = nrArticolo, cliente = fun.getCliente(nrArticolo, df_articoli), valuta = fun.getValuta(nrArticolo, df_ricavi_b), p = fun.articoloFinale(nrArticolo, df_valuta_b, df_valuta_c, df_costo_b, df_costo_c, df_ricavi_b, df_ricavi_c))
-
+    return render_template("articoloSpecifico.html", codiceArticolo='ART000045', cliente="pippo", p=(((0.2761111111111111,17.708333333333332,18.0,323.71999999999997,40,36.91869841571008,1476.7479366284033),(17.65463917525773, 97.0, 1712.5, 88, 34.52013674643542, 3037.7720336863167,1516.7479366284033))))
 
 @app.route("/scostamentoVolumi")
 def scostamentoVolumi():
-    return render_template("scostamentoVolumi.html", p=((407000,67840,242200,80000,16960),(366300,61056,217000,80000,7264),(363500,60160,217000,80000,6340),(370000,63920,220780,80000,5300)))
+    return render_template("scostamentoVolumi.html", p=((407000,456000),(456000,407000)))
+
+@app.route("/scostamentoMix")
+def scostamentoMix():
+    return render_template("scostamentoMix.html", p=(('ART000045',61056,407000,456000),('ART000086',61056,363500,220780),('ART000015',63920,407000,6340)))
 
 
 if __name__ == '__main__':
